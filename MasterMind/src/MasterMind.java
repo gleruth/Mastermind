f2e3109257ec15baa49e44471811ac03816023b9
Gonzague Leruth <gleruth@exclin.com>
2018-03-16 21:46:23 +0000

    A user interface is added

--- MasterMind/src/MasterMind.java
+++ MasterMind/src/MasterMind.java
@@ -1,4 +1,18 @@
+//Random numbers library
 import java.util.Random;
+//JavaFx UI
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.Group;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.*;
+import javafx.scene.shape.CullFace;
+import javafx.scene.shape.Sphere;
+import javafx.stage.Stage;
 
 class Colors {
 	// Set of available colors
@@ -11,9 +25,11 @@ class Colors {
 	}
 }
 
-public class MasterMind {
+public class MasterMind extends Application {
 
 	public static void main(String[] args) {
+		//UI
+		launch(args);
 		//Create a random Color
 		Colors color = new Colors();
 		color.color = Colors.randomColor();
@@ -62,5 +78,44 @@ public class MasterMind {
 		consoleResult += Integer.toString(1);
 		consoleResult += "/10 | ";
 		System.out.println(consoleResult);
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		//Button btn = new Button("Hello");
+		//btn.setOnAction(new EventHandler<ActionEvent>() {
+		//	public void handle (ActionEvent event) {
+		//		System.out.println("Hellow");
+		//	}
+		//});
+		int gameRows = 10;
+		int gameColumns = 4;
+
+		Group root = new Group();
+		Sphere[][] grid = new Sphere[gameRows][gameColumns];
+		for (int i = 0; i < gameRows; i++) {
+			for (int j = 0; j < gameColumns; j++) {
+			    Sphere emptySphere = new Sphere();
+			    emptySphere.setRadius(20.0);
+			    emptySphere.setCullFace(CullFace.FRONT);
+			    emptySphere.setTranslateX(50+50*j);
+			    emptySphere.setTranslateY(50+50*i);
+			    root.getChildren().add(emptySphere);
+				grid[i][j] = emptySphere;
+			}
+		}
+		for (int j = 0; j < gameColumns; j++) {
+			PhongMaterial material = new PhongMaterial();
+		    material.setDiffuseColor(Color.ORANGE);
+		    material.setSpecularColor(Color.BLACK);
+		    grid[0][j].setMaterial(material);
+			grid[0][j].setCullFace(CullFace.BACK);
+		}
+		
+		Scene scene = new Scene(root, 800, 600);
+		arg0.setTitle("MasterMind");
+		arg0.setScene(scene);
+		arg0.show();
+		
 	}
 }
