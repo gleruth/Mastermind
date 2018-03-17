e177e86c70f6e63871deb573477e7351a7009b28
Gonzague Leruth <gleruth@exclin.com>
2018-03-17 13:32:51 +0000

    Cleaning code by migrating to separate classes

--- MasterMind/src/MasterMind.java
+++ MasterMind/src/MasterMind.java
@@ -17,10 +17,10 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 class Colors {
-	// Set of available colors
+	// Define a set of available colors
 	enum Color {R, J, B, O, V, N}
 	Color color;
-	// Function: Random selection of a color
+	// Select a random color
 	static Color randomColor() {
 	    int pick = new Random().nextInt(Color.values().length);
 	    return Color.values()[pick];
@@ -29,218 +29,315 @@ class Colors {
 
 class GameParameters {
 	int tentative;
+	int gameRows;
+	int gameColumns;
+	int possibleColors;
+	int screenWidth;
+	int screenHeight;
+	String gameTitle;
+	
+	void initParameters() {
+		tentative = 0;
+		gameRows = 10;
+		gameColumns = 4;
+		possibleColors = 6;
+		screenWidth = 800;
+		screenHeight = 600;
+		gameTitle = "MasterMind Game - Gonzague Leruth\'s Premium Edition";
+	}
 }
 
-public class MasterMind extends Application {
-
-	public static void main(String[] args) {
-		//UI
-		launch(args);
-		//Create a random Color
-		Colors color = new Colors();
-		color.color = Colors.randomColor();
-		//Create the computer selection of colors
-		Colors[] computerSelection;
+class ComputerSelectionManager {
+	Colors[] computerSelection;
+	// Create a computer random selection of colors
+	Colors[] createComputerSelection() {
 		computerSelection = new Colors[4];
 		for (int i = 0; i < computerSelection.length; i++) {
+			Colors color = new Colors();
+			color.color = Colors.randomColor();
 			computerSelection[i] = color;
 			System.out.println(computerSelection[i].color);
-			color.color = Colors.randomColor();
-	     }
-		//User chooses colors
-		Colors[] userSelection;
-		userSelection = new Colors[4];
-		userSelection[0] = color;
-		userSelection[1] = color;
-		userSelection[2] = color;
-		userSelection[3] = color;
-		//Compute how many colors are correct
-		int correctScore = 0;
-		for (int i = 0; i < computerSelection.length; i++) {
-			for (int j = 0; j < userSelection.length; j++) {
-				if (computerSelection[i] == userSelection[j]) {
-					correctScore++;
+		}
+		return computerSelection;
+	}
+}
+
+class PlaceSphereManager{
+	// Place a sphere on the screen
+	Sphere placeSphere(Group root, double radius, boolean visible, int x, int y) {
+		Sphere sphere = new Sphere();
+	    sphere.setRadius(radius);
+	    if (visible) {
+	    	sphere.setCullFace(CullFace.BACK);
+	    } else {
+	    	sphere.setCullFace(CullFace.FRONT);
+	    }
+	    sphere.setTranslateX(x);
+	    sphere.setTranslateY(y);
+	    root.getChildren().add(sphere);
+	    return sphere;
+	}
+	void assignColor(Color color, Sphere sphere) {
+	    PhongMaterial material = new PhongMaterial();
+	    material.setSpecularColor(Color.BLACK);
+	    if (color == Color.BLACK) {
+	    	material.setSpecularColor(Color.WHITE);
+	    }
+	    material.setDiffuseColor(color);
+	    sphere.setCullFace(CullFace.BACK);
+	    sphere.setMaterial(material);
+	}
+	void assignColorEvent(Color color, Sphere sphereToClick, Sphere sphereToAffect) {
+		assignColor(color, sphereToClick);
+		sphereToClick.setOnMouseClicked(event -> {
+			assignColor(color, sphereToAffect);
+		});
+	}
+}
+
+class SphereGridManager{
+	// Create a grid of spheres
+	Sphere[][] createSphereGrid(Group root, int columns, int rows, double radius, boolean visible, int x_orig, int y_orig, int x_spacing, int y_spacing) {
+		Sphere[][] grid = new Sphere[rows][columns];
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				PlaceSphereManager placeSphereManager = new PlaceSphereManager();
+				Sphere sphere = placeSphereManager.placeSphere(root, radius, visible, x_orig + j*x_spacing, y_orig + i*y_spacing);
+				grid[i][j] = sphere;
+			}
+		}
+		return grid;
+	}
+}
+
+class GameGridManager{
+	// Create the game grid of spheres
+	Sphere[][] createGameGrid(Group root, GameParameters param) {
+		SphereGridManager gridManager = new SphereGridManager();
+		Sphere[][] grid = gridManager.createSphereGrid(root, param.gameColumns, param.gameRows, 20.0, false, 50, 50, 50, 50);
+		return grid;
+	}
+}
+
+class ScoreGridManager{
+	// Create the score grid of spheres
+		Sphere[][] createScoreGrid(Group root, GameParameters param) {
+			SphereGridManager gridManager = new SphereGridManager();
+			Sphere[][] grid = gridManager.createSphereGrid(root, param.gameColumns, param.gameRows, 5.0, false, 0, 0, 0, 0);
+			for (int i = 0; i < param.gameRows; i++) {
+				for (int j = 0; j < param.gameColumns; j++) {
+				    grid[i][j].setTranslateX(240+(j % 2)*20);
+				    if (j >= 2) {
+				    	grid[i][j].setTranslateY(60 + 50*i);
+				    } else {
+				    	grid[i][j].setTranslateY(40 + 50*i);
+				    }
+				}
+			}	
+			return grid;
+		}
+}
+
+class UserSelectionLineManager{
+	// Create the user selection line of spheres
+	Sphere[] createUserSelectionLine(Group root, int columns, double radius, boolean visible, int x_orig, int y_orig, int x_spacing) {
+		Sphere[] line = new Sphere[columns];
+		for (int i = 0; i < columns; i++) {
+			PlaceSphereManager placeSphereManager = new PlaceSphereManager();
+			Sphere sphere = placeSphereManager.placeSphere(root, radius, visible, x_orig + i*x_spacing, y_orig);
+			line[i] = sphere;
+		}
+		return line;	
+	}
+}
+
+class PickGridManager{
+	// Create the grid of color spheres to be picked
+		Sphere[][] createPickGrid(Group root, GameParameters param, Sphere[] userSelectionLine) {
+			SphereGridManager gridManager = new SphereGridManager();
+			Sphere[][] grid = gridManager.createSphereGrid(root, param.possibleColors, param.gameColumns, 20.0, true, 0, 0, 0, 0);
+			
+			for (int i = 0; i < param.possibleColors; i++) {
+				for (int j = 0; j < param.gameColumns; j++) {
+					if ((i % 2) == 0) {
+						grid[j][i].setTranslateX(375+110*j);
+						grid[j][i].setTranslateY(125+25*i);
+				    } else {
+				    	grid[j][i].setTranslateX(425+110*j);
+				    	grid[j][i].setTranslateY(125+25*(i-1));
+				    }
+					PlaceSphereManager placeSphereManager = new PlaceSphereManager();
+					switch (i) {
+		            	case 0:  placeSphereManager.assignColorEvent(Color.RED, grid[j][i], userSelectionLine[j]);
+		                    break;
+		            	case 1:  placeSphereManager.assignColorEvent(Color.YELLOW, grid[j][i], userSelectionLine[j]);
+	                		break;
+		            	case 2:  placeSphereManager.assignColorEvent(Color.BLUE, grid[j][i], userSelectionLine[j]);
+	                		break;
+		            	case 3:  placeSphereManager.assignColorEvent(Color.ORANGE, grid[j][i], userSelectionLine[j]);
+	                		break;
+		            	case 4:  placeSphereManager.assignColorEvent(Color.GREEN, grid[j][i], userSelectionLine[j]);
+	                		break;
+		            	case 5:  placeSphereManager.assignColorEvent(Color.BLACK, grid[j][i], userSelectionLine[j]);
+	                		break;
+				    	}
 				}
 			}
-		}
-		//Compute how many colors are correct and at the right place
-		int correctPlace = 0;
-		for (int i = 0; i < computerSelection.length; i++) {
-			if (computerSelection[i] == userSelection[i]) {
-				correctPlace++;
-			}
-		}
-		//Display status on console
-		String consoleResult = "";
-		consoleResult = "|";
-		for (int i = 0; i < userSelection.length; i++) {
-			consoleResult += userSelection[i].color;
-		}
-		consoleResult += "| ";
-		consoleResult += Integer.toString(correctScore);
-		consoleResult += " | ";
-		consoleResult += Integer.toString(correctPlace);
-		consoleResult += " | ";
-		consoleResult += Integer.toString(1);
-		consoleResult += "/10 | ";
-		System.out.println(consoleResult);
-	}
+			return grid;
+		}		
+}
 
-	@Override
-	public void start(Stage arg0) throws Exception {
-		int gameRows = 10;
-		int gameColumns = 4;
-		int possibleColors = 6;
-		GameParameters param = new GameParameters();
-		param.tentative = 0;
-
-		Group root = new Group();
-		Sphere[][] grid = new Sphere[gameRows][gameColumns];
-		for (int i = 0; i < gameRows; i++) {
-			for (int j = 0; j < gameColumns; j++) {
-			    Sphere emptySphere = new Sphere();
-			    emptySphere.setRadius(20.0);
-			    emptySphere.setCullFace(CullFace.FRONT);
-			    emptySphere.setTranslateX(50+50*j);
-			    emptySphere.setTranslateY(50+50*i);
-			    root.getChildren().add(emptySphere);
-				grid[i][j] = emptySphere;
-			}
-		}
-		for (int j = 0; j < gameColumns; j++) {
-			PhongMaterial material = new PhongMaterial();
-		    material.setDiffuseColor(Color.ORANGE);
-		    material.setSpecularColor(Color.BLACK);
-		    grid[0][j].setMaterial(material);
-			grid[0][j].setCullFace(CullFace.BACK);
-		}
-		
-		Sphere[] userProposition = new Sphere[gameColumns];
-		for (int i = 0; i < gameColumns; i++) {
-			Sphere emptySphere = new Sphere();
-		    emptySphere.setRadius(40.0);
-		    emptySphere.setCullFace(CullFace.FRONT);
-		    emptySphere.setTranslateX(400+110*i);
-		    emptySphere.setTranslateY(50);
-		    root.getChildren().add(emptySphere);
-			userProposition[i] = emptySphere;
-		}
-		Sphere[][] userChoices = new Sphere[possibleColors][gameColumns];
-		for (int i = 0; i < possibleColors; i++) {
-			for (int j = 0; j < gameColumns; j++) {
-			    Sphere emptySphere = new Sphere();
-			    emptySphere.setRadius(20.0);
-			    emptySphere.setCullFace(CullFace.BACK);
-			    if ((i % 2) == 0) {
-			    	emptySphere.setTranslateX(375+110*j);
-				    emptySphere.setTranslateY(125+25*i);
-			    } else {
-			    	emptySphere.setTranslateX(425+110*j);
-				    emptySphere.setTranslateY(125+25*(i-1));
-			    }
-			    //emptySphere.setTranslateX(400+100*j);
-			    //emptySphere.setTranslateY(150+50*i);
-			    PhongMaterial material = new PhongMaterial();
-			    material.setSpecularColor(Color.BLACK);
-			    switch (i) {
-	            case 0:  material.setDiffuseColor(Color.RED);
-	                    break;
-	            case 1:  material.setDiffuseColor(Color.YELLOW);
-                		break;
-	            case 2:  material.setDiffuseColor(Color.BLUE);
-                		break;
-	            case 3:  material.setDiffuseColor(Color.ORANGE);
-                		break;
-	            case 4:  material.setDiffuseColor(Color.GREEN);
-                		break;
-	            case 5:  material.setDiffuseColor(Color.BLACK);
-	            		material.setSpecularColor(Color.WHITE);
-                		break;
-			    }
-			    emptySphere.setMaterial(material);
-			    emptySphere.setCullFace(CullFace.BACK);
-			    switch (j) {
-	            case 0:  emptySphere.setOnMouseClicked(event -> {
-					userProposition[0].setCullFace(CullFace.BACK);
-					userProposition[0].setMaterial(material);
-				});
-	                    break;
-	            case 1:  emptySphere.setOnMouseClicked(event -> {
-					userProposition[1].setCullFace(CullFace.BACK);
-					userProposition[1].setMaterial(material);
-				});
-                		break;
-	            case 2:  emptySphere.setOnMouseClicked(event -> {
-					userProposition[2].setCullFace(CullFace.BACK);
-					userProposition[2].setMaterial(material);
-				});
-                		break;
-	            case 3:  emptySphere.setOnMouseClicked(event -> {
-					userProposition[3].setCullFace(CullFace.BACK);
-					userProposition[3].setMaterial(material);
-				});
-                		break;
-			    }
-			    root.getChildren().add(emptySphere);
-				userChoices[i][j] = emptySphere;
-			}
-		}
-		Sphere[][] userFeedback = new Sphere[gameRows][gameColumns];
-		for (int i = 0; i < gameRows; i++) {
-			for (int j = 0; j < gameColumns; j++) {
-			    Sphere emptySphere = new Sphere();
-			    emptySphere.setRadius(5.0);
-			    emptySphere.setCullFace(CullFace.FRONT);
-			    emptySphere.setTranslateX(240+(j % 2)*20);
-			    if (j >= 2) {
-			    	emptySphere.setTranslateY(60 + 50*i);
-			    } else {
-			    	emptySphere.setTranslateY(40 + 50*i);
-			    }
-			    PhongMaterial material = new PhongMaterial();
-			    material.setSpecularColor(Color.BLACK);
-			    material.setDiffuseColor(Color.RED);
-			    emptySphere.setMaterial(material);
-			    root.getChildren().add(emptySphere);
-			    userFeedback[i][j] = emptySphere;
-			}
-		}
+class ExtraUIElementsManager{
+	void addExtraUIElements(Group root, GameParameters param, Sphere[][] gameGrid, Sphere[] userSelectionLine){
 		Button btn = new Button("Confirm your selection");
 		btn.setTranslateX(500);
 		btn.setTranslateY(275);
 		btn.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle (ActionEvent event) {
-				for (int j = 0; j < gameColumns; j++) {
-				    grid[param.tentative][j].setMaterial(userProposition[j].getMaterial());
-					grid[param.tentative][j].setCullFace(CullFace.BACK);
-				}
-				param.tentative++;
-				if (param.tentative >= 10) {
-					DropShadow ds = new DropShadow();
-					ds.setOffsetY(3.0f);
-					ds.setColor(Color.color(0.4f, 0.4f, 0.4f));
-
-					Text t = new Text("Game Over");
-					t.setEffect(ds);
-					t.setCache(true);
-					t.setFont(Font.font ("Verdana", 80));
-					t.setFill(Color.RED);
-					t.setX(300);
-					t.setY(550);
-					root.getChildren().add(t);
-				}
+				TentativeManager tentativeManager = new TentativeManager();
+				tentativeManager.newTentative(root, param, gameGrid, userSelectionLine);
 			}
 		});
 		root.getChildren().add(btn);
+	}
+}
+
+class TentativeManager{
+	void newTentative(Group root, GameParameters param, Sphere[][] gameGrid, Sphere[] userSelectionLine) {
+		if (param.tentative < 10) {
+			// if enough attempts left
+			// check validity
+			for (int j = 0; j < param.gameColumns; j++) {
+				// TO CONTINUE
+			}
+			// and take new user proposition into account
+			for (int j = 0; j < param.gameColumns; j++) {
+				gameGrid[param.tentative][j].setMaterial(userSelectionLine[j].getMaterial());
+				gameGrid[param.tentative][j].setCullFace(CullFace.BACK);
+			}
+			param.tentative++;
+		}
+		else {
+			// otherwise, display game over text
+			DropShadow ds = new DropShadow();
+			ds.setOffsetY(3.0f);
+			ds.setColor(Color.color(0.4f, 0.4f, 0.4f));
+			Text t = new Text("Game Over");
+			t.setEffect(ds);
+			t.setCache(true);
+			t.setFont(Font.font ("Verdana", 80));
+			t.setFill(Color.RED);
+			t.setX(300);
+			t.setY(550);
+			root.getChildren().add(t);
+		}
+	}
+}
+
+class UIElementsManager{
+	void positionUIElements(Group root, GameParameters param, Colors[] computerSelection) {
+		// Game grid
+		GameGridManager gameGridManager = new GameGridManager();
+		Sphere[][] grid = gameGridManager.createGameGrid(root, param);
+		// Score grid
+		ScoreGridManager scoreGridManager = new ScoreGridManager();
+		Sphere[][] scoreGrid = scoreGridManager.createScoreGrid(root, param);
+		// User new selection line
+		UserSelectionLineManager userSelectionLineManager = new UserSelectionLineManager();
+		Sphere[] userSelectionLine = userSelectionLineManager.createUserSelectionLine(root, param.gameColumns, 40.0, false, 400, 50, 110);	
+		// Colors to be picked grid
+		PickGridManager pickGridManager = new PickGridManager();
+		Sphere[][] pickGrid = pickGridManager.createPickGrid(root, param, userSelectionLine);
+		// Extra UI elements
+		ExtraUIElementsManager extraUIelementsManager = new ExtraUIElementsManager();
+		extraUIelementsManager.addExtraUIElements(root, param, grid, userSelectionLine);
 		
-		//userChoices[0][0].setOnMouseClicked(event -> {
-		//		userProposition[1].setCullFace(CullFace.BACK);
-		//});
 		
-		Scene scene = new Scene(root, 800, 600);
-		arg0.setTitle("MasterMind");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//Create a random Color
+		Colors color = new Colors();
+		color.color = Colors.randomColor();
+				//User chooses colors
+				Colors[] userSelection;
+				userSelection = new Colors[4];
+				userSelection[0] = color;
+				userSelection[1] = color;
+				userSelection[2] = color;
+				userSelection[3] = color;
+				//Compute how many colors are correct
+				int correctScore = 0;
+				for (int i = 0; i < computerSelection.length; i++) {
+					for (int j = 0; j < userSelection.length; j++) {
+						if (computerSelection[i] == userSelection[j]) {
+							correctScore++;
+						}
+					}
+				}
+				//Compute how many colors are correct and at the right place
+				int correctPlace = 0;
+				for (int i = 0; i < computerSelection.length; i++) {
+					if (computerSelection[i] == userSelection[i]) {
+						correctPlace++;
+					}
+				}
+				//Display status on console
+				String consoleResult = "";
+				consoleResult = "|";
+				for (int i = 0; i < userSelection.length; i++) {
+					consoleResult += userSelection[i].color;
+				}
+				consoleResult += "| ";
+				consoleResult += Integer.toString(correctScore);
+				consoleResult += " | ";
+				consoleResult += Integer.toString(correctPlace);
+				consoleResult += " | ";
+				consoleResult += Integer.toString(1);
+				consoleResult += "/10 | ";
+				System.out.println(consoleResult);
+		
+		
+		
+		
+	}
+}
+
+class MainScreenManager{
+	static void generateScreen(Stage arg0) {
+		// Set game parameters
+		GameParameters param = new GameParameters();
+		param.initParameters();
+		
+		// Create the computer selection of colors
+		ComputerSelectionManager computerSelectionManager = new ComputerSelectionManager();
+		Colors[] computerSelection = computerSelectionManager.createComputerSelection();
+		
+		// Set the screen
+		Group root = new Group();
+		UIElementsManager uiElementsManager = new UIElementsManager();
+		uiElementsManager.positionUIElements(root, param, computerSelection);
+		Scene scene = new Scene(root, param.screenWidth, param.screenHeight);
+		arg0.setTitle(param.gameTitle);
 		arg0.setScene(scene);
 		arg0.show();
-		
+	}
+}
+
+public class MasterMind extends Application {
+	//Main function
+	public static void main(String[] args) {
+		launch(args);
+	}
+	//JavaFX UI management
+	public void start(Stage arg0) throws Exception {
+		MainScreenManager.generateScreen(arg0);
 	}
 }
